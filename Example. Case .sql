8d0cf79ac6705dbbb05fb0a3ab49dc47b9dc8500
Ruben Marinez <me@rubenmarinez.com>
2024-12-16 11:48:28 -0600

    Create Example. Case .sql

---
+++ Example. Case .sql
@@ -0,0 +1,8 @@
+--El siguiente ejemplo indica como utilizar al sentencia CASE en SQL Server. 
+ 
+CASE 
+    WHEN condition1 THEN result1 
+    WHEN condition2 THEN result2 
+    WHEN conditionN THEN resultN 
+    ELSE result 
+END;
