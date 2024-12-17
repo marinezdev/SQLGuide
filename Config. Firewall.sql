d48897b69d5b4befca98b5b17a060d656e68221d
Ruben Marinez <me@rubenmarinez.com>
2024-12-16 22:06:59 -0600

    Create Config. Firewall.sql

---
+++ Config. Firewall.sql
@@ -0,0 +1,9 @@
+SQLConectionNetwork  
+Conexión a SQL Server SA202 
+  
+se configuró Firewall de windows (puerto 1433 tanto entrante como saliente) 
+adicional se agregó el parametro dentro del protocols de SQL Server para que funcione con el 1433 
+  
+https://www.youtube.com/watch?v=GwWyOtQzfsc
+
+NOTA: En mis notas se encuentra el manual con imagenes
