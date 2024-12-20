bfb3b8dd1d5d8e10cf4db0c47629cae2a6a4e46b
Ruben Marinez <me@rubenmarinez.com>
2024-12-19 18:49:07 -0600

    Update Example. EMail Send.sql

--- Example. EMail Send.sql
+++ Example. EMail Send.sql
@@ -1,12 +1,27 @@
--- La configuración del envío de correo se encuentra documentada en las notas
-
+-- INFORMATION: Referencia para la configuración de Envio de Mails. 
+-- INFORMATION: https://database.guide/how-to-send-emails-from-sql-server-t-sql/ 
+-- INFORMATION: https://www.sqlshack.com/es/como-poder-configurar-la-base-de-datos-del-correo-en-sql-server/ 
+ 
+  -- NOTA Importante: para utilizar el siguiente código se tuvo que haber creado el perfil y la cuenta de mail en SQL Server. Se explica en el link anterior. 
+USE msdb 
+GO 
+ 
+declare @contenido nvarchar(MAX); 
+declare @fechaEnvio nvarchar(30); 
+ 
+set @fechaEnvio = CONVERT(nvarchar, GETDATE(), 103) + ' ' + CONVERT(nvarchar, GETDATE(), 108); 
+print @fechaEnvio; 
+ 
+set @contenido = '<h1>WARNING</h1><hr/><br/><small>Enviamos mensaje de prueba....<br/><br/>' + @fechaEnvio + '</small>'; 
+--print @contenido; 
+ 
 EXEC msdb.dbo.sp_send_dbmail  
-  @profile_name = 'SQL Adminisrtration' 
-  ,@recipients = 'ruben.marines@asae.com.mx' 
+  @profile_name='SQL Administrador' 
+  ,@recipients='ruben.marines@asae.com.mx' 
   ,@copy_recipients = '' 
-  ,@subject = 'test email sending from SQL Server' 
-  ,@body = 'prueba del envío de correo electronicos desde SQL Server' 
+  ,@subject='Warning [x9999]' 
+  ,@body=@contenido 
   ,@body_format = 'HTML' 
   ,@from_address = 'Administración de Base de Datos <ruben.marines@asae.com.mx>' 
-
-
+  ,@file_attachments = 'F:\WEBSITES\certificate.rmf' 
+  ,@importance = 'High' 
