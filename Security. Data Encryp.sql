fd05ef7af7049edb7760aed4476ba102d93e0b09
Ruben Marinez <me@rubenmarinez.com>
2024-12-19 18:46:03 -0600

    Create Security. Data Encryp.sql

---
+++ Security. Data Encryp.sql
@@ -0,0 +1,4 @@
+-- El siguiente link explica como utilizar la encriptación de datos en SQL Server 2016  
+-- https://www.c-sharpcorner.com/article/always-encrypted-in-sql-server-2016-step-by-step-guide/ 
+ 
+ 
