d3e22178c0743e5ce73c81276c4878bff51179b6
Ruben Marinez <me@rubenmarinez.com>
2024-12-19 19:05:50 -0600

    Create Mantto. Check Performance.sql

---
+++ Mantto. Check Performance.sql
@@ -0,0 +1,4 @@
+-- El siguiente link contiene información muy interesante para la verificación del rendimiento de SQL Server. 
+
+https://docs.microsoft.com/es-es/sql/relational-databases/performance/monitor-and-tune-for-performance?view=sql-server-ver15 
+https://solutioncenter.apexsql.com/es/cuando-como-y-por-que-volver-a-generar-y-reorganizar-indices-de-sql-server/ 
